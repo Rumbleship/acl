11e847c126aa5853c752ad4880d4d1d11660e639
Isaac Sparling <223606+isparling@users.noreply.github.com>
2019-07-01 19:12:37 -0700

    :zap: improve exports

---
+++ src/authorizer.ts
@@ -0,0 +1,94 @@
+import * as jwt from 'jsonwebtoken';
+import {
+  PermissionsMatrix,
+  IAllowedQuery,
+  RumbleshipAccessTokenSignature,
+  Roles,
+  Actions,
+  Resource,
+  PermissionSource,
+  RolesAt
+} from './types';
+export class Authorizer {
+  private roles: RolesAt;
+  // private user?: string;
+  // private name?: string;
+  // private client?: string;
+  // private exp?: Date;
+
+  constructor(
+    private accessToken: string,
+    private secret: string,
+    private sourceType: PermissionSource = PermissionSource.MATRIX,
+    private matrix: PermissionsMatrix = {
+      [Roles.ADMIN]: {},
+      [Roles.USER]: {},
+      [Roles.PENDING]: {}
+    }
+  ) {
+    if (!this.accessToken) {
+      throw new Error('`accessToken` is required by Authorizer');
+    }
+    if (!this.secret) {
+      throw new Error('`secret` is required by Authorizer');
+    }
+    if (this.sourceType === PermissionSource.MATRIX && !this.matrix) {
+      throw new Error('Cannot use sourceType `MATRIX` without specifying the matrix to reference');
+    }
+    const { /* user, client, name, exp, */ roles } = jwt.verify(
+      this.accessToken,
+      this.secret
+    ) as RumbleshipAccessTokenSignature;
+    // this.name = name;
+    // this.user = user;
+    this.roles = roles;
+    // this.client = client;
+    // this.exp = exp;
+  }
+
+  allowed(options: IAllowedQuery): boolean {
+    const { to: permission, from, match: attribute = 'hashid', against: authorizable } = options;
+    let access = false;
+
+    switch (this.sourceType) {
+      case PermissionSource.DECORATOR:
+        throw new Error('decorators not currently supported');
+      case PermissionSource.MATRIX:
+        (Object.entries(this.roles) as [[Roles, string[]]]).forEach(
+          ([role, identifiersWithPermissions]) => {
+            const permissions: Actions[] = this.permissionsBeingConsidered(
+              role,
+              authorizable,
+              from
+            );
+            const identifier = (authorizable as any)[attribute];
+            if (
+              this.isUserSysAdmin() ||
+              (identifiersWithPermissions.includes(identifier) && permissions.includes(permission))
+            ) {
+              access = true;
+            }
+          }
+        );
+        break;
+    }
+
+    return access;
+  }
+
+  isUserSysAdmin(): boolean {
+    return Object.values(this.roles).every((identifiersWithPermissions: string[]) =>
+      identifiersWithPermissions.includes('*')
+    );
+  }
+  private permissionsBeingConsidered(role: Roles, authorizable: any, from?: Resource): Actions[] {
+    const permissionable = from || authorizable.constructor.name;
+    if (permissionable === 'Object') {
+      throw new Error('Cannot permission on generic `Object`');
+    }
+    if (!this.matrix[role]) {
+      return [];
+    }
+    return this.matrix[role][permissionable];
+  }
+}
