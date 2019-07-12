d76e2e569e746be2048508dd018e4d73e18e0e6c
Isaac Sparling <223606+isparling@users.noreply.github.com>
2019-07-11 17:12:37 -0700

    :bullettrain_front: better comments

--- lib/authorizer.d.ts
+++ lib/authorizer.d.ts
@@ -12,18 +12,13 @@ export declare class Authorizer {
     /**
      * Type-GraphQL compatible method that singularly answers the question:
      * "Given the accessToken that this Authorizer represents:
-     *    - can I take an Action against an Authorizable object, given a set of Permissions
-     *      defined by a TypeGraphQL Authorized decorator"
+     *    - can I take an Action against an Authorizable object, given a set of Permissions"
      * @param action The action under consideration, typically query|mutation in GQL
-     * @param authorizable In a RESTful world, an object whose entiriety should be authorized.
-     *                      In A GQL world, an object with fields being individually authorized.
+     * @param authorizable The record being authorized before being returned to the requesting User
      * @param matrix The permission matrix defined in the GQL model via Authorized decorator
-     *                --or-- A list of roles that generically have access without inflecting
-     *                  on the identity of a record|resource. Useful for authorizing APIs in TGQL
-     *                  e.g. mutators and queries
-     * @param attribute? The attribute that should be used to index into the `authorizable`
-     * @param resource? An override if `authorizeable.constructor.name` is not the group of
-     *                    actions to permission agaiXnst
+     * @param attribute? Explicitly indicate how to index into the `authorizable`
+     * @param resource? Explicitly indicate which group in the matrix should be permissioned
+     *                  against.
      */
     can(action: Actions, authorizable: object, matrix: PermissionsMatrix[], attribute?: string, resource?: Resource): boolean;
     inScope(...scopeOrScopeArray: Array<Scopes | Scopes[]>): boolean;
