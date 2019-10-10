ddd7360bc55bfe74e39a0992da88943e768e2693
Nicholas Smith <nicktsmith7@gmail.com>
2019-10-10 10:23:51 -0700

    use Reflect rather than square bracket indexing into the authorizer

--- src/test/decorators.test.ts
+++ src/test/decorators.test.ts
@@ -95,6 +95,6 @@ describe('Scenario: A Subclass can decorate a method that invokes the authorizer
 describe('Scenario: An instance of Authorizer is instantiated and is attached to the metadata, which is logged', () => {
   test('The Authorizer class is instantiated and the secret property is removed', () => {
     const authorizer = new Authorizer(userOfOwnerRelatedToAuthorizable, SECRET);
-    expect(authorizer.toJson()).not.toContain(authorizer['secret']);
+    expect(authorizer.toJson()).not.toContain(Reflect.get(authorizer, 'secret'));
   });
 });
